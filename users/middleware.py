ff8dc8ed3da33090b8d6fbf1ff02e540e8f6ac6a
wujiawei36 <3150763913@qq.com>
2026-08-24 14:52:36 +0800

    Fixed something so that the server can get the real ip of the user

--- users/middleware.py
+++ users/middleware.py
@@ -1,3 +1,5 @@
+from utils.get_ip import get_ip
+
 class SessionInfoMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
@@ -5,6 +7,6 @@ class SessionInfoMiddleware:
     def __call__(self, request):
         if request.user.is_authenticated:
             # 每次请求都更新 session 里的 IP 和 UA
-            request.session['ip_address'] = request.META.get('REMOTE_ADDR', '')
+            request.session['ip_address'] = get_ip(request)
             request.session['user_agent'] = request.META.get('HTTP_USER_AGENT', '')
         return self.get_response(request)
---
+++ utils/get_ip.py
@@ -0,0 +1,5 @@
+def get_ip(request):
+    xff = request.META.get('HTTP_X_FORWARDED_FOR')
+    if xff:
+        return xff.split(',')[0].strip()
+    return request.META.get('REMOTE_ADDR')
