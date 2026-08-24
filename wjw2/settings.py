5e966bbd2cd5464759f24662d1b5727e720f9461
wujiawei36 <3150763913@qq.com>
2026-08-24 16:00:39 +0800

    Added some safety middlewares

--- wjw2/settings.py
+++ wjw2/settings.py
@@ -52,8 +52,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-	# 'index.middleware.IPBlockMiddleware',
-	# 'index.middleware.RequestBlockingMiddleware',
+	'index.middleware.IPBlockMiddleware',
+	'index.middleware.RequestBlockingMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
