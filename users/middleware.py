d36a342f139d79f5c584ad58154a2bb4f7cfe177
wujiawei36 <3150763913@qq.com>
2026-08-24 14:59:08 +0800

    Try to fix some problems

--- users/middleware.py
+++ users/middleware.py
@@ -3,10 +3,11 @@ from utils.get_ip import get_ip
 class SessionInfoMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
+        print('=== SessionInfoMiddleware loaded ===')  # 加这行
 
     def __call__(self, request):
         if request.user.is_authenticated:
-            # 每次请求都更新 session 里的 IP 和 UA
+            print(f'=== IP: {get_ip(request)} ===')  # 加这行
             request.session['ip_address'] = get_ip(request)
             request.session['user_agent'] = request.META.get('HTTP_USER_AGENT', '')
         return self.get_response(request)
